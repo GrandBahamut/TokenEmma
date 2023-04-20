410e710cd874695e0b8f8423a58020da6f55a949
GrandBahamut <grandbahamutzero@hotmail.com>
2023-04-19 21:49:32 -0400

    BHM1

---
+++ contract-084bf347bd.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable@4.8.3/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.3/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.3/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable@4.8.3/proxy/utils/UUPSUpgradeable.sol";
+
+contract Bahamut is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("Bahamut", "BHM");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, 12000000000000000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
